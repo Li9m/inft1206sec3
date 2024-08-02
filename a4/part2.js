870a96266d2592a216cbfa324162d5006c3d10b0
Liam <169002740+Li9m@users.noreply.github.com>
2024-08-02 18:14:37 -0400

    part 2 completed with comments

---
+++ a4/part2.js
@@ -0,0 +1,41 @@
+// JavaScript for Part 2 //
+// Liam Springgay //
+// 2024-08-02 //
+// INFT-1206 //
+
+btn.setAttribute("class", xxx);
+btn.textContent = xxx;
+overlay.style.backgroundColor = xxx;
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rbga(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rbga(0,0,0,0)';
+    }
+});
+
